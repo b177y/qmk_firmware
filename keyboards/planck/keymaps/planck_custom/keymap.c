763e8798099c1693da95652e227ad0b669c3068e
b177y <34008579+b177y@users.noreply.github.com>
2022-09-26 22:01:11 +0100

    add meh to space key

--- keyboards/planck/keymaps/planck_custom/keymap.c
+++ keyboards/planck/keymaps/planck_custom/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_A, KC_R, MT(MOD_LALT, KC_S), MT(MOD_LGUI,KC_T), KC_G,        
                                                         KC_NO, KC_NO,       KC_M, MT(MOD_RGUI, KC_N), MT(MOD_RALT, KC_E), KC_I, KC_O,
             KC_Z, KC_X, KC_C, KC_D, KC_V,               KC_NO, KC_NO,       KC_K, KC_H, TD(DANCE_2), TD(DANCE_1), TD(DANCE_3),
-            KC_NO, KC_NO, KC_NO, KC_SPC, OSM(MOD_LSFT), KC_NO, KC_NO,       KC_LCTL, OSL(1), KC_NO, KC_NO, KC_NO
+            KC_NO, KC_NO, KC_NO, MT(MOD_MEH,KC_SPACE), OSM(MOD_LSFT), KC_NO, KC_NO,       KC_LCTL, OSL(1), KC_NO, KC_NO, KC_NO
         ),
     // Layer 1 - 'one shot' - punctuation and to other layers
 	[1] = LAYOUT_ortho_4x12(
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[2] = LAYOUT_ortho_4x12(
             KC_ESC, KC_MPLY, KC_MRWD, KC_MFFD, KC_UNDS, KC_NO, KC_NO,       KC_PMNS, KC_P7, KC_P8, KC_P9, KC_BSPC,
             KC_TAB, KC_NO, KC_VOLD, KC_VOLU, KC_PLUS,   KC_NO, KC_NO,       KC_PEQL, KC_P4, KC_P5, KC_P6, KC_ENT,
-            TO(4), KC_PAST, KC_BRID, KC_BRIU, KC_PDOT,  KC_NO, KC_NO,       KC_NO, KC_P1, KC_P2, KC_P3, KC_NO,
+            TO(4), KC_PAST, KC_BRID, KC_BRIU, KC_PDOT,  KC_NO, KC_NO,       KC_P0, KC_P1, KC_P2, KC_P3, KC_NO,
             KC_NO, KC_NO, KC_NO, TO(0), KC_LGUI,        KC_NO, KC_NO,       KC_RALT, KC_NO, KC_NO, KC_NO, KC_NO
         ),
     // Layer 3 - navigation
