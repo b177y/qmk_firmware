cc29432369ebf3a3b3c637d1c7ebeafd9fed306a
b177y <34008579+b177y@users.noreply.github.com>
2022-09-27 10:20:17 +0100

    add to diagram

--- keyboards/planck/keymaps/planck_custom/keymap.c
+++ keyboards/planck/keymaps/planck_custom/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[3] = LAYOUT_ortho_4x12(
             KC_ESC, KC_WH_L, KC_MS_U, KC_WH_R, KC_NO,   KC_NO, KC_NO,       KC_WBAK, KC_NO, KC_NO, KC_WFWD, KC_BSPC,
             KC_TAB, KC_MS_L, KC_MS_D, KC_MS_R, KC_LGUI, KC_NO, KC_NO,       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_ENT,
-            KC_GRV, KC_MS_R, KC_WH_U, KC_WH_D, KC_DEL,  KC_NO, KC_NO,       KC_MS_L, KC_ACL0, KC_ACL1, KC_ACL2, KC_TRNS,
+            KC_GRV, KC_MS_R, KC_WH_U, KC_WH_D, KC_DEL,  KC_NO, KC_NO,       KC_MS_L, KC_ACL0, KC_ACL1, KC_ACL2, KC_NO,
             KC_NO, KC_NO, KC_NO, TO(0), KC_NO,          KC_NO, KC_NO,       KC_RALT, TO(1), KC_NO, KC_NO, KC_NO
         ),
     // Layer 4 - keyboard config
